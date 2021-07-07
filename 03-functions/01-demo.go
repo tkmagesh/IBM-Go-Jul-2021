37b8e602665dde9ba894868fa7571ed9a2b8efa9
Magesh Kuppan <tkmagesh77@gmail.com>
2021-07-07 14:08:50 +0530

    functions - basics

---
+++ 03-functions/01-demo.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	result := add(100, 200)
+	fmt.Println(result)
+	//fmt.Println(divide(100, 7))
+	quotient, remainder := divide(100, 7)
+	fmt.Println(quotient, remainder)
+}
+
+/*
+func add(x int, y int) int {
+	return x + y
+}
+*/
+
+func add(x, y int) int {
+	return x + y
+}
+
+/*
+func divide(x, y int) (int, int) {
+	quotient := x / y
+	remainder := x % y
+	return quotient, remainder
+}
+*/
+
+func divide(x, y int) (quotient, remainder int) {
+	quotient = x / y
+	remainder = x % y
+	return
+}
