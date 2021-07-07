1fae9f1d22b58785267c80605c420b7f88bfb622
Magesh Kuppan <tkmagesh77@gmail.com>
2021-07-07 16:25:20 +0530

    closures - assignment - solution

--- 03-functions/05-demo.go
+++ 03-functions/05-demo.go
@@ -3,12 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	counter := getCounter()
-	fmt.Println(counter()) //=> 1
-	fmt.Println(counter()) //=> 2
-	fmt.Println(counter()) //=> 3
-	fmt.Println(counter()) //=> 4
-	fmt.Println(counter()) //=> 5
+	/*
+		counter := getCounter()
+		fmt.Println(counter()) //=> 1
+		fmt.Println(counter()) //=> 2
+		fmt.Println(counter()) //=> 3
+		fmt.Println(counter()) //=> 4
+		fmt.Println(counter()) //=> 5
+	*/
+
+	increment, decrement := getCounters()
+	fmt.Println(increment())
+	fmt.Println(increment())
+	fmt.Println(increment())
+	fmt.Println(decrement())
+	fmt.Println(decrement())
+	fmt.Println(decrement())
+	fmt.Println(decrement())
+
 }
 
 func getCounter() func() int { //=> step : 1
@@ -18,4 +30,17 @@ func getCounter() func() int { //=> step : 1
 		return count
 	}
 	return counter //=> step : 5
+}
+
+func getCounters() (func() int, func() int) {
+	count := 0
+	increment := func() int {
+		count++
+		return count
+	}
+	decrement := func() int {
+		count--
+		return count
+	}
+	return increment, decrement
 }
