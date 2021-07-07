d28c87a1b5d32524e1fbbee4c4f4e3990ebe1b07
Magesh Kuppan <tkmagesh77@gmail.com>
2021-07-07 11:57:45 +0530

    Initial commit

---
+++ 01-hello-world/program.go
@@ -0,0 +1,15 @@
+//package declaration
+package main
+
+//import other packages
+import "fmt"
+
+//package level variables / types
+
+//package init function
+
+func main() {
+	fmt.Println("Hello World")
+}
+
+//other functions
