b6dfbd4cb448277978ecd2848fb5c1c2907b5028
Magesh Kuppan <tkmagesh77@gmail.com>
2021-07-07 12:17:53 +0530

    variable declaration syntax options

--- 01-hello-world/program.go
+++ 01-hello-world/program.go
@@ -9,7 +9,57 @@ import "fmt"
 //package init function
 
 func main() {
-	fmt.Println("Hello World")
+
+	/*
+		var message string
+		message = "Hello World"
+	*/
+
+	/*
+		var message string = "Hello World"
+	*/
+
+	/*
+		var message = "Hello World"
+	*/
+
+	//the following syntax works only in a function
+	message := "Hello World"
+	fmt.Println(message)
+
+	//using multiple variables
+	/*
+		var name string
+		var msg string
+		name = "Magesh"
+		msg = "Welcome to Golang"
+	*/
+	/*
+		var (
+			name string
+			msg  string
+		)
+	*/
+	/*
+		var (
+			name, msg string
+		)
+	*/
+	/*
+		var name, msg string
+		name = "Magesh"
+		msg = "Welcome to Golang"
+	*/
+
+	/*
+		var name, msg string = "Magesh", "Welcome to Golang"
+	*/
+
+	/*
+		var name, msg = "Magesh", "Welcome to Golang"
+	*/
+	name, msg := "Magesh", "Welcome to Golang"
+	fmt.Println(name, msg)
 }
 
 //other functions
