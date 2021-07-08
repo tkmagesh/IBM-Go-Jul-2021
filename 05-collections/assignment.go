b2f43dd8222d056ed4635d9acf6d46c0ec6db7ec
Magesh Kuppan <tkmagesh77@gmail.com>
2021-07-08 12:58:41 +0530

    assignment solution

---
+++ 05-collections/assignment.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	str := "Aliquip magna irure eiusmod sit elit ipsum incididunt qui labore fugiat culpa ut commodo irure Excepteur duis Lorem dolor adipisicing dolor officia mollit voluptate Consectetur exercitation occaecat laboris voluptate amet aliqua esse ex anim enim Elit exercitation officia tempor consectetur consequat reprehenderit dolor labore ex veniam minim fugiat consequat Aliquip velit voluptate qui aute enim Magna labore sint nulla magna ipsum"
+	words := strings.Split(str, " ")
+	wordsCountBySize := getWordsCountBySize(words)
+	maxCount, maxSize := getMaxCountBySize(wordsCountBySize)
+	fmt.Printf("The max occurence of words by size of the word = %d (word size = %d)\n", maxCount, maxSize)
+}
+
+func getWordsCountBySize(words []string) map[int]int {
+	wordsCountBySize := map[int]int{}
+	for _, word := range words {
+		wordsCountBySize[len(word)]++
+	}
+	return wordsCountBySize
+}
+
+func getMaxCountBySize(wordsBySize map[int]int) (int, int) {
+	maxCount := 0
+	maxSize := 0
+	for size, count := range wordsBySize {
+		if count > maxCount {
+			maxCount = count
+			maxSize = size
+		}
+	}
+	return maxCount, maxSize
+}
--- 05-collections/program.go
+++ 05-collections/program.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 func main() {
@@ -101,9 +100,4 @@ func main() {
 	for key, value := range cityRanks {
 		fmt.Printf("Value at [%s] is %d\n", key, value)
 	}
-
-	str := "Aliquip magna irure eiusmod sit elit ipsum incididunt qui labore fugiat culpa ut commodo irure Excepteur duis Lorem dolor adipisicing dolor officia mollit voluptate Consectetur exercitation occaecat laboris voluptate amet aliqua esse ex anim enim Elit exercitation officia tempor consectetur consequat reprehenderit dolor labore ex veniam minim fugiat consequat Aliquip velit voluptate qui aute enim Magna labore sint nulla magna ipsum"
-	words := strings.Split(str, " ")
-
-	fmt.Println(len(words))
 }
