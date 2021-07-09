32fa8a83fe52d2d2b198697cfffae2555026743d
Magesh Kuppan <tkmagesh77@gmail.com>
2021-07-09 14:20:14 +0530

    using Mutex

---
+++ 10-concurrency/03-demo.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var result int
+
+var wg *sync.WaitGroup = &sync.WaitGroup{}
+var mutex *sync.Mutex = &sync.Mutex{}
+
+func main() {
+	wg.Add(1)
+	go add(100, 200)
+	wg.Wait()
+	fmt.Println(result)
+}
+
+func add(x, y int) {
+	mutex.Lock()
+	result = x + y
+	mutex.Unlock()
+	wg.Done()
+}
